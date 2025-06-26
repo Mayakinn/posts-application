1cfdad9e73a1a8dd387a1e271d7081e721e0ed40
Maja V <maja.vinceviciute@teltonika.lt>
2025-06-26 08:37:00 +0300

    fixes

--- src/views/AuthorView.vue
+++ src/views/AuthorView.vue
@@ -5,12 +5,16 @@ import { getAuthors } from "@/api/AuthorService";
 import AuthorList from "@/components/authorComponents/AuthorList.vue";
 
 const loading = ref<boolean>(true);
+const empty = ref<boolean>(false);
 const authors = ref<Author[]>([]);
 
 onMounted(async () => {
   await getAuthors().then(function (response) {
     loading.value = false;
     authors.value = response;
+    if (authors.value == undefined) {
+      empty.value = true;
+    }
   });
 });
 </script>
@@ -18,6 +22,7 @@ onMounted(async () => {
 <template>
   <div class="Author">
     <div v-if="loading" class="title">Loading author information</div>
+    <div v-else-if="empty" class="title">Author list is empty</div>
     <div v-else><AuthorList :Authors="authors" /></div>
   </div>
 </template>
