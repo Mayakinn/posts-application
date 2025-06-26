9839ecac3575809b7ac61036a1552199595dffc2
Maja V <maja.vinceviciute@teltonika.lt>
2025-06-26 09:19:17 +0300

    Notif fix

--- src/store/NotificationStore.ts
+++ src/store/NotificationStore.ts
@@ -2,17 +2,17 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { NotificationType } from "@/typings/interface/NotificationType";
 import { type Notification } from "@/typings/interface/Notification";
+import { v4 as uuidv4 } from "uuid";
 
-export const useNotificationStore = defineStore("status", () => {
-  const idCounter = ref<number>(0);
+export const useNotificationStore = defineStore("notification", () => {
   const Notifs = ref<Notification[]>([]);
 
-  function deleteNotification(id: number) {
+  function deleteNotification(id: string) {
     Notifs.value = Notifs.value.filter((n) => n.id !== id);
   }
 
   function newNotification(message: string, status: NotificationType) {
-    const id = idCounter.value++;
+    const id = uuidv4();
     const notification = { message, status, id };
     Notifs.value.push(notification);
     setTimeout(() => {
