06a14f2a8041d68fc2d2aa0c2a46ccb10b9009c5
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-02 15:06:19 +0300

    FORM FIXED

--- src/components/formComponents/AuthorCreateForm.vue
+++ src/components/formComponents/AuthorCreateForm.vue
@@ -19,30 +19,30 @@ async function handleCreateAuthor() {
 </script>
 
 <template>
-  <header class="modal-card-head">
-    <p class="modal-card-title">Author Creation</p>
-  </header>
-  <div class="modal-card-body">
-    Author name:
-    <input
-      v-model="inputedName"
-      type="text"
-      class="input is-primary is-rounded"
-      required
-      style="max-width: 1000px"
-    />
-    Author surname:
-    <input
-      v-model="inputedSurname"
-      type="text"
-      class="input is-primary is-rounded"
-      required
-      style="max-width: 1000px"
-    /><br />
-  </div>
-  <footer class="modal-card-foot">
-    <button @click="handleCreateAuthor" class="button is-primary">
-      Add new Author
-    </button>
-  </footer>
+  <form @submit="handleCreateAuthor">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Author Creation</p>
+    </header>
+    <div class="modal-card-body">
+      Author name:
+      <input
+        v-model="inputedName"
+        type="text"
+        class="input is-primary is-rounded"
+        required
+        style="max-width: 1000px"
+      />
+      Author surname:
+      <input
+        v-model="inputedSurname"
+        type="text"
+        class="input is-primary is-rounded"
+        required
+        style="max-width: 1000px"
+      /><br />
+    </div>
+    <footer class="modal-card-foot">
+      <input class="button" type="submit" value="Submit" />
+    </footer>
+  </form>
 </template>
